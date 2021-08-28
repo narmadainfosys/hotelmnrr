2d4eb789f2da6a2a298ef0d74d727f2df7556c83
arcy405 <arcy405@gmail.com>
2021-08-28 08:39:30 +0545

    rails admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+
   # get 'cats/index'
   # get 'cat/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "cats#index"
 
   resources :cats
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
 end
