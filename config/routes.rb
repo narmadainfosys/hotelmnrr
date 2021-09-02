9d77407f62b34de185f378aadb18fa6765c8a84a
arcy405 <arcy405@gmail.com>
2021-09-02 17:03:44 +0545

    cloudinry

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
+  resources :posts
   resources :records
   get 'dogs/index'
-  resources :posts
   devise_scope :user do
     get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
     get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
