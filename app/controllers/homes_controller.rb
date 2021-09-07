5a5b5f88a0e36330b65c4f077c4902958e39ba9e
arcy405 <arcy405@gmail.com>
2021-09-07 15:58:29 +0545

    room added

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,5 +2,7 @@ class HomesController < ApplicationController
   before_action :authenticate_user!
   def index
     @current_user = current_user.role
+    @foods = Food.all
+    @rooms = Room.all
   end
 end
